9df3b5ed2de09db06394aeca660758e2110262fd
AnuprabhSingh <127375629+AnuprabhSingh@users.noreply.github.com>
2023-12-25 11:54:21 +0530

    added design.js and made some changes to app.js file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './css/App.css';
+import Dashboard from './pages/Dashboard';
+import Design from "./components/Design";
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+      <Design />
+    </>
+  );
+}
+
+export default App;
